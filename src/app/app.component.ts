db33024625697808231bae6e15e0c46ad00a3dd0
Mandy <Lmandy239@gmail.com>
2024-09-14 13:59:26 -0400

    initial push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    HomeComponent,
+    CommonModule,
+    FormsModule,
+    NavigationComponent,
+    RouterOutlet
+  ],
+
+
 })
+
 export class AppComponent {
   title = 'PersonalSite';
 }
